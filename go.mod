99063a8341b78767573ea064b5d7c56829380419
toby lorne <toby@toby.codes>
2020-09-12 10:03:57 +0100

    main: basic html templating, metrics, health
    Signed-off-by: toby lorne <toby@toby.codes>

--- go.mod
+++ go.mod
@@ -4,7 +4,16 @@ go 1.14
 
 require (
 	github.com/jarcoal/httpmock v1.0.6
+	github.com/meatballhat/negroni-logrus v1.1.0
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
+	github.com/phyber/negroni-gzip v0.0.0-20180113114010-ef6356a5d029
+	github.com/prometheus/client_golang v1.7.1
 	github.com/salsita/go-pivotaltracker v2.1.0+incompatible
+	github.com/sethvargo/go-signalcontext v0.1.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/unrolled/render v1.0.3
+	github.com/unrolled/secure v1.0.8
+	github.com/urfave/negroni v1.0.0
+	github.com/zbindenren/negroni-prometheus v0.1.1
 )
--- main.go
+++ main.go
@@ -1,24 +1,84 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"syscall"
+	"time"
 
 	"github.com/tlwr/todo-aggregator/agg/pivotal"
-	. "github.com/tlwr/todo-aggregator/todo"
+	"github.com/tlwr/todo-aggregator/todo"
+
+	nlogrus "github.com/meatballhat/negroni-logrus"
+	"github.com/phyber/negroni-gzip/gzip"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sethvargo/go-signalcontext"
+	"github.com/sirupsen/logrus"
+	"github.com/unrolled/render"
+	nsecure "github.com/unrolled/secure"
+	"github.com/urfave/negroni"
+	nprom "github.com/zbindenren/negroni-prometheus"
 )
 
 func main() {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetOutput(os.Stdout)
+	logger.SetLevel(logrus.InfoLevel)
+
+	todos := []todo.Todo{}
+
 	pivotalTrackerProjects := []string{"1133984"}
-
-	todos := []Todo{}
-
 	pivotalTodos, err := pivotal.FetchPivotalTodos(pivotalTrackerProjects)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	todos = append(todos, pivotalTodos...)
 
 	for _, todo := range todos {
-		log.Printf("pivotal: %s %s", todo.Name(), todo.URL())
+		logger.Printf("pivotal: %s %s", todo.Name(), todo.URL())
 	}
+
+	renderer := render.New(render.Options{
+		Directory: "templates",
+	})
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "healthy")
+	})
+
+	mux.Handle("/metrics", promhttp.Handler())
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		renderer.HTML(w, http.StatusOK, "todos", todos)
+	})
+
+	n := negroni.New()
+	n.Use(negroni.NewRecovery())
+	n.Use(nlogrus.NewMiddlewareFromLogger(logger, "web"))
+	n.Use(gzip.Gzip(gzip.DefaultCompression))
+	n.Use(negroni.HandlerFunc(nsecure.New().HandlerFuncWithNext))
+	n.Use(nprom.NewMiddleware("todo-aggregator"))
+	n.Use(negroni.NewStatic(http.Dir("public")))
+	n.UseHandler(mux)
+
+	ctx, cancel := signalcontext.On(syscall.SIGTERM)
+	defer cancel()
+
+	server := &http.Server{Addr: ":8080", Handler: n}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	<-ctx.Done()
+
+	ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+	os.Exit(0)
 }
